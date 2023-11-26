5813adae0d5b75342936279965177bc423f786e8
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-11-26 23:43:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function switchTab(newTab) {
         oldTab.classList.add("current-tab");
 
         if(!searchForm.classList.contains("active")) {
+            
             //kya search form wala container is invisible, if yes then make it visible
+            
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
             searchForm.classList.add("active");
